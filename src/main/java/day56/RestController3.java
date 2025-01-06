bdcb788e11a7f7fa1e939ab5414440ca664abcb5
itdanjalog <kgs2072@naver.com>
2025-01-06 20:46:00 +0900

    day56

---
+++ src/main/java/day56/RestController3.java
@@ -0,0 +1,21 @@
+package day56;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/day56/rest3")
+public class RestController3 {
+
+    @GetMapping("")
+    public boolean getRest3(){ return true; }
+
+    @PostMapping("")
+    public boolean postRest3(){ return false; }
+
+    @PutMapping("")
+    public boolean putRest3(){ return true; }
+
+    @DeleteMapping("")
+    public boolean deleteRest4(){ return false; }
+
+} // class end
