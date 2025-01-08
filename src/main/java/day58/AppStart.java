fa976b13236f1a1cfc86bbe6228e51320d9cfd04
itdanjalog <kgs2072@naver.com>
2025-01-08 20:59:12 +0900

    day58

--- src/main/java/day58/AppStart.java
+++ src/main/java/day58/AppStart.java
@@ -14,12 +14,22 @@ public class AppStart {
         SpringApplication.run( AppStart.class );
     }
 }
-
 @Controller // @ : 미리 만들어진 코드 및 정보들을 알려주는 역할 (스티커)
 class ViewController{
 
-    @GetMapping("")
+    @GetMapping("") // 템플릿 파일 반환하는 HTTP URL 정의
     public String index(){
-        return "/day58/index.html";
+        return "/day58/index.html"; // index.html템플릿 파일 반환
+    } // f end
+
+    @GetMapping("/layout") // 매핑/연결 할 HTTP URL 정의
+    public String index2(){
+        return "/day58/layout/index.html"; // index.html 템플릿 파일 반환
     }
-}
+
+    @GetMapping("/student")
+    public String index3(){
+        return "/day58/student/index.html"; // index.html 템플릿 파일 반환
+    }
+
+} // class end
