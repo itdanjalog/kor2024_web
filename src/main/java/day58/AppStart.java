416be46e90df12654980021e7af5ca47c8914356
itdanjalog <kgs2072@naver.com>
2025-01-08 21:40:45 +0900

    day58

--- src/main/java/day58/AppStart.java
+++ src/main/java/day58/AppStart.java
@@ -39,7 +39,29 @@ class ViewController{
     }
     // [3] student 전체 학생 점수 조회 페이지
     @GetMapping("/student/list")
-    
+    public String list(){
+        return "/day58/student/list.html";
+    }
+    // [4] student 개별 학생 점수 조회 페이지
+    @GetMapping("/student/info")
+    public String info(){
+        return "/day58/student/info.html";
+    }
+    // [5] student 점수 수정 페이지
+    @GetMapping("/student/update")
+    public String update(){
+        return "/day58/student/update.html";
+    }
+    // [6] student 점수 삭제 페이지
+    @GetMapping("/student/delete")
+    public String delete(){
+        return "/day58/student/delete.html";
+    }
+
+
+
+
+
 } // class end
 
 
