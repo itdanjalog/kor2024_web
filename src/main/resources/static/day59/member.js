9f8582a1508073d1bac59a9d2ffcce25c9c5f8a3
itdanjalog <kgs2072@naver.com>
2025-01-09 21:57:58 +0900

    day59

--- src/main/resources/static/day59/member.js
+++ src/main/resources/static/day59/member.js
@@ -9,10 +9,8 @@ function signup(){ console.log('signup() open')
     const mnameInput = document.querySelector('.mnameInput')
     const mphoneInput = document.querySelector('.mphoneInput')
     //(2) DOM의 VALUE 입력값 반환 
-    const mid = midInput.value;
-    const mpwd = mpwdInput.value;
-    const mname = mnameInput.value;
-    const mphone = mphoneInput.value;
+    const mid = midInput.value;         const mpwd = mpwdInput.value;
+    const mname = mnameInput.value;     const mphone = mphoneInput.value;
     //(3) 여러개 데이터를 하나의 타입으로 묶어주기 = 객체화
     const info = { mid : mid , mpwd : mpwd , mname : mname , mphone : mphone } 
     //(4) fetch 
@@ -40,3 +38,24 @@ function signup(){ console.log('signup() open')
 
 
 // 2. login 함수 
+function login(){
+    // 1. 
+    const midInput = document.querySelector('.midInput');
+    const mpwdInput = document.querySelector('.mpwdInput');
+    // 2. 
+    const mid = midInput.value
+    const mpwd = mpwdInput.value
+    // 3. URL 뒤로 ? 부터는 쿼리스트링 방식 , 매개변수=값&매개변수=값
+    fetch( `/login?mid=${mid}&mpwd=${mpwd}` )
+        .then( response => response.json() )
+        .then( data => {
+            if( data == true ){
+                alert('로그인 성공');
+                // 로그인 성공시 메인페이지로 이동 
+                location.href="/"
+            }else{
+                alert('로그인 실패')
+            }
+        })
+        .then( error => {console.log(error); } )
+} //f end 
