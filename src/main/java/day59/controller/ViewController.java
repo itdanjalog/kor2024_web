921ffcc86e15636c2ac747e14e94f307b3766733
itdanjalog <kgs2072@naver.com>
2025-01-10 19:14:32 +0900

    day59

--- src/main/java/day59/controller/ViewController.java
+++ src/main/java/day59/controller/ViewController.java
@@ -7,19 +7,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ViewController {
     // + 템플릿 파일들을 (VIEW) 반환하는 메소드들을 모아둔 클래스
 
-    // 1. 메인페이지 반환하는 get매핑 함수
+    // 1. 메인페이지 반환하는 get매핑 함수           // http://localhost:8080/
     @GetMapping("")
     public String index(){
         return "/day59/index.html";
     }
 
-    // 2. 회원가입 페이지 반환 하는 get매핑 함수
+    // 2. 회원가입 페이지 반환 하는 get매핑 함수   //http://localhost:8080/signup
     @GetMapping("/signup")
     public String signup(){
         return "/day59/signup.html";
     }
 
-    // 3. 로그인 페이지 반환 하는 get매핌함수
+    // 3. 로그인 페이지 반환 하는 get매핌함수     // http://localhost:8080/login
     @GetMapping("/login")
     public String login(){
         return "/day59/login.html";
