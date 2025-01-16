22eda594cde06f327f82fde851a72ef9f9f49c2f
itdanjalog <itdanja@kakao.com>
2025-01-16 19:51:18 +0900

    day64

---
+++ src/main/java/day64/객체연관관계/회원.java
@@ -0,0 +1,26 @@
+package day64.객체연관관계;
+
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// 롬복
+@NoArgsConstructor@AllArgsConstructor
+@Setter@Getter@Builder @ToString
+public class 회원 {
+    private int 회원번호;
+    private String 회원아이디;
+    private String 회원이름;
+    // 양방향 : 회원객체는 여러개 게시물을 작성할 수 있으므로 List타입
+    @Builder.Default // 빌더패턴 사용시 초기값이 대입되지 않는 멤버변수에 대해서는 초기값을 자동으로 대입한다.
+    @ToString.Exclude // ToString() 제외하는 멤버변수 정의
+    List<게시물> 작성한게시물 = new ArrayList<>();
+    @Builder @ToString.Exclude
+    List<댓글> 작성한댓글 = new ArrayList<>();
+    // 양방향 주의할점 : 두 객체 서로 참조할때는 순환참조(무한호출) 주의해야한다.
+    // 해결방안1 : ToString 메소드를 사용하지 않는다.
+    // 해결방안2 : 양방향 멤버변수만 ToString 메소드를 사용하지 않는다. // @ToString.Exclude
+    // @ToString 란? Object클래스의 메소드로써 객체의 멤버변수 값 를 반환 함수
+        // ToString() 사용하지 않으면 객체 참조시 주소값이 반환 (기본값)
+}
