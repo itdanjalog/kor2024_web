94e8a642a47ed9c5773ae61f7c20c4ee8df999e2
itdanjalog <itdanja@kakao.com>
2025-01-16 21:26:12 +0900

    day64

--- src/main/java/day64/JPA연관관계/model/entity/BoardEntity.java
+++ src/main/java/day64/JPA연관관계/model/entity/BoardEntity.java
@@ -33,12 +33,29 @@ public class BoardEntity extends BaseTime {
     @JoinColumn( name="mno") // FK필드의 이름 정의
     private MemberEntity memberEntity;
 
-    // 양방향
-    @OneToMany( mappedBy = "boardEntity")
+    // 양방향      // board가 삭제/수정/저장 되면 reply도 같이 삭제/수정/저장 , .getReply() 할때 댓글 데이터를 가져온다.(지연로딩)
+    @OneToMany( mappedBy = "boardEntity" , cascade = CascadeType.ALL , fetch = FetchType.LAZY )
     @ToString.Exclude @Builder.Default
-    List< ReplyEntity> list = new ArrayList<>();
-
+    List< ReplyEntity> replyEntityList = new ArrayList<>();
 }
+/*
+    - 영속성 제약조건 ( cascade , fetch ) : PK와 FK 필드 관계 옵션
+        @OneToMany( cascade = CascadeType.XXX ) : 서로 연관된 객체들 간의 영향을 주는 방법 설정
+        1. cascade = CascadeType.ALL           : 부모[PK] 가 삭제/수정 되면 자식[FK] 도 같이 삭제/수정        , REMOVE+MERGE+PERSIST
+        2. cascade = CascadeType.REMOVE        : 부모[PK] 가 삭제 되면 자식[FK] 도 같이 삭제
+        3. cascade = CascadeType.MERGE         : 부모[PK] 가 수정 되면 자식[FK] 도 같이 수정
+        4. cascade = CascadeType.PERSIST       : 부모[PK] 가 저장 될때 참조하는 자식[FK] 도 같이 저장된다.
+        5. cascade = CascadeType.REFRESH       : 부모[PK] 가 수정되면 자동으로 자식[FK] 값도 다시 불러온다.[새로고침]
+        6. cascade = CascadeType.DETACH        : 영속성(연관관계) 제거
+
+    - fetch : 양방향일때 참조되는 엔티티 불러오는 로딩 방식
+        1. fetch = FetchType.EAGER              : 즉시 로딩(기본값) , find 함수 사용할때 참조되는 모든 객체를 불러온다.
+            특징 : 연관된 엔티티를 즉시 모두 불러온다.
+            단점 : 불필요한 엔티티가 가져올 수 있다.
+        2. fetch = FetchType.LAZY               : 지연 로딩        , getXXX 할때 참조 객체를 불러온다. ( 메모리 효율적이다 )
+            특징 : 연관된 엔티티를 호출할 때( .getXXX ) 데이터를 불러온다.
+            장점 : 초기 로딩 시 빠르다.
+ */
 
 
 
--- src/main/java/day64/JPA연관관계/model/entity/MemberEntity.java
+++ src/main/java/day64/JPA연관관계/model/entity/MemberEntity.java
@@ -22,14 +22,12 @@ public class MemberEntity extends BaseTime {
     @Column( columnDefinition = "varchar(50)" , unique = true , nullable = false )
     private String memail; // 회원이메일주소
     // 양방향 : 데이터베이스 지원하지 않지만 , ORM 에서 지원한다.
-    @OneToMany( mappedBy = "memberEntity") // 하나(PK)가 다수(FK)에게 , 1 : M ,
-    // mappedBy="단방향의멤버변수명" : 자바에서만 양방향 사용하겠다는 설정
+    @OneToMany( mappedBy = "memberEntity" , cascade = CascadeType.ALL , fetch = FetchType.LAZY ) // 하나(PK)가 다수(FK)에게 , 1 : M ,// mappedBy="단방향의멤버변수명" : 자바에서만 양방향 사용하겠다는 설정
     @ToString.Exclude // 양방향에서 순환 참조를 대비 하기 위한 toString 사용 제외
     @Builder.Default // 빌더 패턴 사용시 자동으로 초기값을 주입 하는 어노테이션
     private List< BoardEntity > boardEntityList = new ArrayList<>();
-
     // 양방향 :
-    @OneToMany(  mappedBy = "memberEntity" )
+    @OneToMany(  mappedBy = "memberEntity" , cascade = CascadeType.ALL , fetch = FetchType.LAZY )
     @ToString.Exclude @Builder.Default
     private List<ReplyEntity> entities = new ArrayList<>();
 
